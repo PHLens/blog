6e5c3cc93410d36851bdb2c0d079d8ffcd210bf8
lph <1033957037@qq.com>
2020-03-16 00:22:27 +0800

    update

---
+++ autopull/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    folder : "site",
+    port : 9091,
+    token : "a123456"
+};
---
+++ autopull/start_blog.sh
@@ -0,0 +1,5 @@
+echo `date`
+cd /usr/local/lib/site
+echo start pull from github 
+git pull https://github.com/PHLens/phlens.github.io.git
+echo start build..
